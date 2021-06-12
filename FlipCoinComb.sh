97066a9dbdb05843a494f2709f70385f072bef99
abuzar7972 <abushaikh7972@gmail.com>
2021-06-12 19:57:22 +0530

    Doublet combination with percentage

--- FlipCoinComb.sh
+++ FlipCoinComb.sh
@@ -1,20 +1,38 @@
 #!/bin/bash -x
-heads=0
-tails=0
+HH=1
+HT=1
+TH=1
+TT=1
 read -p "Enter number of loops for execution:: " n
 for ((counter=1; counter<=$n; counter++ ))
 do
-randomCheck=$((RANDOM%2))
-if [ $randomCheck -eq 1 ]
+	randomCheck=$((RANDOM%4))
+	if [ $randomCheck -eq 0 ]
+		then
+		echo "HH"
+		HH=$(($HH+1))
+	fi
+	if [ $randomCheck -eq 1 ]
 	then
-	echo "Heads"
-	heads=$(($heads+1))
-	else
-	echo "Tails"
-	tails=$(($tails+1))
-fi
+		echo "HT"
+		HT=$(($HT+1))
+	fi
+	if [ $randomCheck -eq 2 ]
+     	then
+		echo "TH"
+       		TH=$(($TH+1))
+	fi
+	if [ $randomCheck -eq 3 ]
+        then
+		echo "TT"
+       		TT=$(($TT+1))
+	fi
 done
-per=$((($heads*100)/$n))
-echo "Heads Percentage is equal to:: " $per
-per1=$((($tails*100)/$n))
-echo "Tails Percentage is equal to:: " $per1
+per0=$((($HH*100)/$n))
+echo "HH Percentage is equal to:: " $per0
+per1=$((($HT*100)/$n))
+echo "HT Percentage is equal to:: " $per1
+per2=$((($TH*100)/$n))
+echo "TH Percentage is equal to:: " $per2
+per3=$((($TT*100)/$n))
+echo "HH Percentage is equal to:: " $per3
